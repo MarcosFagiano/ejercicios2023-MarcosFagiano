0a2216984a50943c067d976c9a9de1b1a41fd06b
MarcosFagiano <2112752@ucc.edu.ar>
2023-05-18 19:26:14 -0300

    Ejercicios js convercion de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,7 +1,34 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
+ * Conversion de unidades(metros, yardas, pies y pulgadas)
+ * @method changeUnit
+ * @param {string} id - id de los inputs
+ * @param {number} value - valor de los inputs
+ * @return
  */
+
+function changeUnit(id, value){
+    if(isNaN(value)){
+        alert('Se ingreso un valor invalido.');
+        document.unit.unid_metro.value= "";
+        document.unit.unid_pulgada.value= "";
+        document.unit.unid_pie.value= "";
+        document.unit.unid_yarda.value= "";
+    }else if(id=="metro"){
+        document.unit.unid_pulgada.value= 39.3701*value;
+        document.unit.unid_pie.value= 3.28*value;
+        document.unit.unid_yarda.value= 1.09*value;
+    }else if(id=="pulgada"){
+        document.unit.unid_metro.value= 0.03*value;
+        document.unit.unid_pie.value= 0.08*value;
+        document.unit.unid_yarda.value= 0.03*value;
+    }else if(id=="pie"){
+        document.unit.unid_metro.value= 0.3*value;
+        document.unit.unid_pulgada.value= 12*value;
+        document.unit.unid_yarda.value= 0.33*value;
+    }else if(id=="yarda"){
+        document.unit.unid_metro.value= 0.91*value;
+        document.unit.unid_pie.value= 3*value;
+        document.unit.unid_pulgada.value= 36*value;
+    }
+
+}
